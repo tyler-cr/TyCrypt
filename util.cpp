4dc1daab7bb99cf0651fe71f3d2cd4f29a199887
unknown <tjcrimando@gmail.com>
2023-07-26 19:34:43 -0700

    Added a simple txt to string converter

--- util.cpp
+++ util.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 
 //simple function that removes all non alphanumeric values in string, and changes original string to it
 void stringstrip(std::string &str){
@@ -14,6 +17,16 @@ std::string newstringstrip(std::string str){
     std::string retstr;
     for(size_t i = 0; i< str.length();i++){
         if (isalnum(str[i])) retstr += str[i];
+    }
+    return retstr;
+}
+
+//simple function for converting txt files to strings
+std::string filetostring(std::string filelocation){
+    std::ifstream examplefile; examplefile.open(filelocation);
+    std::string retstr;
+    while (examplefile.good()){
+        retstr += examplefile.get();
     }
     return retstr;
 }
